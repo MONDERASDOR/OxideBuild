19000cfed3c419d1e8895ef2031bf842f01c008d
NoloopsOffical <140967226+MONDERASDOR@users.noreply.github.com>
2025-04-03 15:29:14 +0200

    Update build.rs

--- src/commands/build.rs
+++ src/commands/build.rs
@@ -1,1 +1,19 @@
+use crate::{config::config_parser::OxideConfig, platform::target::TargetPlatform};
+use clap::Args;
 
+#[derive(Args)]
+pub struct BuildArgs {
+    #[arg(short, long)]
+    profile: Option<String>,
+    #[arg(short, long)]
+    target: Option<String>,
+}
+
+pub fn execute(args: BuildArgs) -> anyhow::Result<()> {
+    let config = OxideConfig::load()?;
+    let target_platform = TargetPlatform::detect_or_select(args.target)?;
+    
+    target_platform.build(&config, args.profile.as_deref())?;
+    
+    Ok(())
+}
